49f712accffc8094d4c972c0f33f2256ebabce57
osamafayyaz3 <osama.fayyaz3@gmail.com>
2024-09-03 16:01:48 -0400

    fix: knex test

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import "dotenv/config";
-import { onDatabaseConnect } from "./config/knex";
+import knex, { onDatabaseConnect } from "./config/knex";
 
 const main = async () => {
     try {
         await onDatabaseConnect();
         console.log("Database is connected");
+        const users = await knex("users");
+        console.log(users);
     } catch (e) {
         console.log(e);
     }
